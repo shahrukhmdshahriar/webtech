366feb5c36c45e68d657beb0de55b8af940c58f3
shahrukhmdshahriar <72984647+shahrukhmdshahriar@users.noreply.github.com>
2021-01-05 07:41:24 +0600

    Create addproduct.php

---
+++ lab9/addproduct.php
@@ -0,0 +1,12 @@
+<html>
+	
+	<body>
+		<?php $name="Tanvir";?>
+		Name: <input type="text" value="<?php echo $name;?>"><br>
+		Category: <input type="text"><br>
+		price: <input type="text"><br>
+		Qty: <input type="text"><br>
+		<button>Add</button>
+	</body>
+</html>
+© 2021 GitHub, Inc.
