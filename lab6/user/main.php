8085c7ed97ebca9f7bb5ee426bc7219e51e5f145
shahrukhmdshahriar <72984647+shahrukhmdshahriar@users.noreply.github.com>
2021-01-05 07:11:47 +0600

    Create main.php

---
+++ lab6/user/main.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Registration system</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="sidebar">
+			<h2>Sidebar</h2>
+			<ul>
+				<li><a href="#">Dashboard</a></li>
+				<li><a href="#">Veiw Profile</a></li>
+				<li><a href="#">Edit Profile</a></li>
+				<li><a href="#">Change Password</a></li>
+				<li><a href="#">logout</a></li>
+			</ul>
+		</div>
+		<div class="main-content">
+			<div class="header">Welcome to your Profile</div>
+			<div class="content">
+				<p>Here goes to content</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+© 2021 GitHub, Inc.
