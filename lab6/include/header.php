f1fa76761f415f6e0b31d2aeac97842d8bf82e95
shahrukhmdshahriar <72984647+shahrukhmdshahriar@users.noreply.github.com>
2021-01-05 07:13:21 +0600

    Create header.php

---
+++ lab6/include/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Regsitration system</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<!-- header start -->
+	<header>
+		<div class="container">
+			<div class="brand">
+				<a href="index.php">Hospital</a>
+			</div>
+			<nav class="menu">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="signup.php">Sign up</a></li>
+					<li><a href="login.php">Login</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<!-- header end -->
