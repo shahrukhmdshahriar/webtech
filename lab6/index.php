d25cd41684e6e0ed4668c48df69a8bf3f4b59462
shahrukhmdshahriar <72984647+shahrukhmdshahriar@users.noreply.github.com>
2021-01-05 07:10:17 +0600

    Create index.php

---
+++ lab6/index.php
@@ -0,0 +1,17 @@
+<?php
+	include('include/header.php');
+?>
+
+	<!-- main start -->
+	<section class="main">
+		<div class="container">
+			<div class="welcome-content">
+				<h1>≋WELCOME≋ ≋TO≋
+				≋OUR≋ ≋HOSPITAL≋ ≋WEBSITE≋</h1>
+			</div>
+		</div>	
+	</section>
+	<!-- main end -->
+<?php
+	include('include/footer.php');
+?>
