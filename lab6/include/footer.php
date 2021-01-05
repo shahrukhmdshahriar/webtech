2c74a0f7968cca7d3cfce3b2788b4a1f91503c6d
shahrukhmdshahriar <72984647+shahrukhmdshahriar@users.noreply.github.com>
2021-01-05 07:13:39 +0600

    Add files via upload

---
+++ lab6/include/footer.php
@@ -0,0 +1,9 @@
+<!-- footer start -->
+	<footer>
+		<div class="container">
+			<p class="credit">&copy; All right reserved by Shahriar</p>
+		</div>
+	</footer>
+	<!-- footer end -->
+</body>
+</html>
